cd18a739e14b0a2cbf1fcf4f73f5879a24a2be97
ednilson <ednilson@medlynx.com.br>
2020-06-30 23:08:30 -0300

    feat: Commit inicial
    Criação da estrutura da API; Configuração com o banco; Criada Controller de autenticação de usuário com rota de registro de usuário

---
+++ src/controller/autenticacaoController.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const Usuario = require('../models/usuario');
+const { Router } = require('express');
+
+const router = express.Router();
+
+router.post('/registrar', async (req, res) => {
+    const { email } = req.body;
+
+    try {
+        if( await Usuario.findOne({ email })){
+            res.status(400).send({error: 'Este email já está sendo usuado!'});
+        }
+
+        const usuario = await Usuario.create(req.body);
+        usuario.senha = undefined;
+        res.send(usuario);
+    } catch (error) {
+        res.status(400).send({error: 'Erro de criação de usuário!'});
+    }
+})
+
+module.exports = app => app.use('/autenticacao', router)
