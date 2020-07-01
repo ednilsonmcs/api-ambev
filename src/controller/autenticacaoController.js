ccd89d0ac768ea47d8acca3078b24d228b622745
ednilson <ednilson@medlynx.com.br>
2020-07-01 17:57:24 -0300

    Refatoração após configuração do eslint

--- src/controller/autenticacaoController.js
+++ src/controller/autenticacaoController.js
@@ -1,9 +1,7 @@
-const express = require('express');
-
 const Usuario = require('../models/usuario');
 const { Router } = require('express');
 
-const router = express.Router();
+const router = Router();
 
 router.post('/registrar', async(req, res) => {
   const { email } = req.body;
--- src/database/index.js
+++ src/database/index.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
 
-mongoose.set('useNewUrlParser', true);
-mongoose.set('useFindAndModify', false);
-mongoose.set('useCreateIndex', true);
 mongoose.connect('mongodb://localhost:27017/ambev', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 });
 mongoose.Promise = global.Promise;
 
